79b31e5d5c1f9f1c329114c2626e9290c3e8e05f
kapil kuthe <kapil.kuthe@heytorus.com>
2024-01-01 10:56:52 +0530

    Basics of GOlang1

--- main.go
+++ main.go
@@ -3,47 +3,61 @@ package main
 import "fmt"
 
 //function eg1
-func add(x, y int) int {
-	return x + y
-}
+// func add(x, y int) int {
+// 	return x + y
+// }
 
 func main() {
 
-	//variable eg.
-	var message string
-	message = "Hello, Go!"
-	fmt.Println(message)
+	// //variable eg.
+	// var message string
+	// message = "Hello, Go!"
+	// fmt.Println(message)
 
-	//constant eg
-	const pi = 3.14
-	fmt.Println(pi)
+	// //constant eg
+	// const pi = 3.14
+	// fmt.Println(pi)
 
-	//function eg1
-	result := add(3, 4)
-	fmt.Println(result)
+	// //function eg1
+	// result := add(3, 4)
+	// fmt.Println(result)
 
-	// if statement
-	x := 10
-	if x > 5 {
-		fmt.Println("x is greater than 5")
-	} else {
-		fmt.Println("x is not greater than 5")
+	// // if statement
+	// x := 10
+	// if x > 5 {
+	// 	fmt.Println("x is greater than 5")
+	// } else {
+	// 	fmt.Println("x is not greater than 5")
+	// }
+
+	// // for loop
+	// for i := 0; i < 5; i++ {
+	// 	fmt.Println(i)
+	// }
+
+	// // switch statement
+	// day := "Monday"
+	// switch day {
+	// case "Monday":
+	// 	fmt.Println("It's Monday!")
+	// case "Tuesday":
+	// 	fmt.Println("It's Tuesday!")
+	// default:
+	// 	fmt.Println("It's some other day.")
+	// }
+
+	// range
+	nums := []int{2, 3, 4}
+	sum := 0
+	for _, num := range nums {
+		sum += num
 	}
+	fmt.Println("sum:", sum)
 
-	// for loop
-	for i := 0; i < 5; i++ {
-		fmt.Println(i)
-	}
-
-	// switch statement
-	day := "Monday"
-	switch day {
-	case "Monday":
-		fmt.Println("It's Monday!")
-	case "Tuesday":
-		fmt.Println("It's Tuesday!")
-	default:
-		fmt.Println("It's some other day.")
+	for i, num := range nums {
+		if num == 3 {
+			fmt.Println("index:", i)
+		}
 	}
 
 }
