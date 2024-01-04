adc97a383331126023b3254a26d6a621c51c2018
kapil kuthe <kapil.kuthe@heytorus.com>
2024-01-04 11:38:17 +0530

    MSSQL CUID basics

--- main.go
+++ main.go
@@ -1,90 +1,37 @@
 package main
 
-//function eg1
-// func add(x, y int) int {
-// 	return x + y
-// }
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/denisenkom/go-mssqldb"
+)
+
+const (
+	server   = "10.112.31.91"
+	port     = 1433
+	user     = "admin"
+	password = "AdM!n@91"
+	database = "test"
+)
+
+var db *sql.DB
+
+func initDB() {
+	var err error
+	connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d;database=%s;",
+		server, user, password, port, database)
+	db, err = sql.Open("sqlserver", connString)
+	if err != nil {
+		log.Fatal("Error creating connection pool: ", err.Error())
+	}
+}
+
+// main.go (continuation)
 
 func main() {
-
-	// //variable eg.
-	// var message string
-	// message = "Hello, Go!"
-	// fmt.Println(message)
-
-	// //constant eg
-	// const pi = 3.14
-	// fmt.Println(pi)
-
-	//byte // alias for uint8
-
-	//rune // alias for int32
-	// represents a Unicode code point
-
-	// Declare a rune
-	// var myRune rune = 'A'
-
-	// // Display the value of the rune
-	// fmt.Println("Value of the rune:", myRune)
-
-	// // Declare a string with runes
-	// myString := "Hello, 世界"
-
-	// // Iterate over the string and print each rune
-	// for _, char := range myString {
-	// 	fmt.Printf("%c ", char)
-	// }
-
-	// //function eg1
-	// result := add(3, 4)
-	// fmt.Println(result)
-
-	// // if statement
-	// x := 10
-	// if x > 5 {
-	// 	fmt.Println("x is greater than 5")
-	// } else {
-	// 	fmt.Println("x is not greater than 5")
-	// }
-
-	// // for loop
-	// for i := 0; i < 5; i++ {
-	// 	fmt.Println(i)
-	// }
-
-	// // switch statement
-	// day := "Monday"
-	// switch day {
-	// case "Monday":
-	// 	fmt.Println("It's Monday!")
-	// case "Tuesday":
-	// 	fmt.Println("It's Tuesday!")
-	// default:
-	// 	fmt.Println("It's some other day.")
-	// }
-
-	// // range eg with arrays
-	// nums := []int{2, 3, 4}
-	// sum := 0
-	// for _, num := range nums {
-	// 	sum += num
-	// }
-	// fmt.Println("sum:", sum)
-
-	// for i, num := range nums {
-	// 	if num == 3 {
-	// 		fmt.Println("index:", i)
-	// 	}
-	// }
-
-	// // range with maps
-	// kvs := map[string]string{"a": "apple", "b": "banana"}
-	// for k, v := range kvs {
-	// 	fmt.Printf("%s -> %s\n", k, v)
-	// }
-
-	// for k := range kvs {
-	// 	fmt.Println("key:", k)
-	// }
+	initDB()
+	defer db.Close()
 
 }
